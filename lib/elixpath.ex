4be36b86811a1af191b8505b92683c211112e7bd
mtannaan <12782884+mtannaan@users.noreply.github.com>
2019-05-24 20:31:43 +0900

    mix new

---
+++ lib/elixpath.ex
@@ -0,0 +1,18 @@
+defmodule Elixpath do
+  @moduledoc """
+  Documentation for Elixpath.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixpath.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixpath_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixpathTest do
+  use ExUnit.Case
+  doctest Elixpath
+
+  test "greets the world" do
+    assert Elixpath.hello() == :world
+  end
+end
