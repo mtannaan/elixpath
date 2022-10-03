4f1d37da3a5efd1d54218fee0d54ba2b8e51d49e
mtannaan <12782884+mtannaan@users.noreply.github.com>
2022-10-03 10:29:19 +0000

    version 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Elixpath.MixProject do
   def project do
     [
       app: :elixpath,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: ">= 1.7.0",
       start_permanent: Mix.env() == :prod,
       description: description(),
