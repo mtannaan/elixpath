de237c9e6734095dd397f1e261a50b978ac048e9
mtannaan <12782884+mtannaan@users.noreply.github.com>
2022-10-03 10:29:08 +0000

    support Elixir >1.8 in mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Elixpath.MixProject do
     [
       app: :elixpath,
       version: "0.1.0",
-      elixir: "~> 1.7 or ~> 1.8",
+      elixir: ">= 1.7.0",
       start_permanent: Mix.env() == :prod,
       description: description(),
       deps: deps(),
